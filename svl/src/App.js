00fe17d12a2300dca12ef77f441948649a5b146e
georgebarreira10 <barreirageorge@gmail.com>
2022-08-23 21:09:14 -0300

    instalamos react router

--- svl/src/App.js
+++ svl/src/App.js
@@ -3,17 +3,25 @@ import './App.css';
 import { FormBook } from './components/FormBook';
 import { NavBar } from './components/NavBar';
 import BookTable from './components/BookTable';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <NavBar />
-      
-      <div className='container'>
 
-      <FormBook />
-      <BookTable />
-    </div>
+
+
+      {/* <FormBook />
+      <BookTable /> */}
+
+      <div className='container'>
+        <Routes>
+          <Route path="/form" element={<FormBook />} />
+          <Route path="/booktable" element={<BookTable />} />
+        </Routes>
+
+      </div >
     </div >
 
   );
--- svl/src/index.js
+++ svl/src/index.js
@@ -6,11 +6,14 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
